05df6bc3f97dae9b8321fe713368597cd1af1380
Neil Wallace <nwallace@microsoft.com>
2010-10-26 13:33:33 +0100

    Merge of beat detection set up

---
+++ RationalVisions/BeatDetection.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//-------------------------------------------------------------------------------------------------------
+class CBeatDetective
+{
+public:
+	CBeatDetective() {}
+
+	void Init();
+	void Update();
+
+private:
+
+};

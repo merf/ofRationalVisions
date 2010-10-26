76d84497ddd3454a2f67120608e8b7e1c33222bf
Neil Wallace <nwallace@microsoft.com>
2010-10-26 14:00:52 +0100

    Changed triangle size

--- RationalVisions/src/TriBandsScene.cpp
+++ RationalVisions/src/TriBandsScene.cpp
@@ -26,13 +26,13 @@ namespace NScene
 		float base_h = h / 2.0f;
 		float curr_w = 0.0f;
 
-		float num = 100.0f;
+		float num = 10.0f;
 
 		float w_inc = w / num;
 		for(float f=0; f<=1.0f; f+=1.0f/num)
 		{
 			ofTranslate(w_inc, 0.0f);
-			float val = -GetSoundEngine().GetShortAverage(f) * h * 0.2f;
+			float val = -GetSoundEngine().GetShortAverage(f) * w_inc;
 			ofTriangle(curr_w, base_h, curr_w + w_inc * 0.5f, base_h + val, curr_w + w_inc, base_h);
 		}
 	}
